bed46de5f62064a3fa6b324dafed6f90decd05b7
Alex <42402960+mralexx11@users.noreply.github.com>
2020-03-16 15:50:11 +0200

    constructors for inc/exp

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
  let budgetController = (() => {
 
+     let Expense = (id, description, value) => {
+         this.id = id;
+         this.description = description;
+         this.value = value;
+     };
+
+     let Income = (id, description, value) => {
+         this.id = id;
+         this.description = description;
+         this.value = value;
+     };
+
+     let data = {
+         allItems: {
+             exp: [],
+             inc: []
+         },
+         totals: {
+             exp: 0,
+             inc: 0
+         }
+     }
  })();
 
 let UIContriller = (() => {
